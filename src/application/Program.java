f4338d4455f623a0b79301c3e605c1a968979572
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-27 17:44:54 +0000

    Inline lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,9 +19,7 @@ public class Program {
 		
 		double min = 100.0;
 		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
+		list.removeIf( p -> p.getPrice() >= min);
 		
 		for (Product p : list) {
 			System.out.println(p);
